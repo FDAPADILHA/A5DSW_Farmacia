db9c0ebd3f1a0d0ce717a0bed60fce859048fde1
matheusmsouza84 <matheusmelchior84@gmail.com>
2020-06-13 22:32:17 -0300

    Inserção de login como administrador e alterações visuais e gerais ao todo

--- admin.php
+++ admin.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <meta charset="utf-8">
-    <title> Cadastro </title>
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <link rel="stylesheet" type="text/css" href="style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <title>Área para Funcionários</title>
 </head>
-    <body>
-    <div class="header">Pharma Store ✙</div>
+<body>
+<div class="header">Pharma Store ✙</div>
         <nav class="navbar navbar-expand-md">
             <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item">
-                    <a href="index.php">
+                        <a href="index.php">
                         <button type="button" id="paginicial" title="Voltar a página Inicial">
                         <svg class="bi bi-house-fill" width="20px" height="20px" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" d="M8 3.293l6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
@@ -27,26 +27,27 @@
             <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
-                     </li>
+                        <button type="button" id="logout" title="Logout">
+                            <svg class="bi bi-person-dash-fill" width="20px" height="20px" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm5-.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5z"/>
+                            </svg>
+                        </button>
+                    </li>
                 </ul>
             </div>
         </nav>
         <div class="content">
             <center>
-                <div class="shadow-lg p-3 mb-5 bg-white rounded" id="divcadastro">  
-                    <h1>Cadastrar Usuário</h1><br>
-                    <form method="POST" action=".php">
-                        <label>Nome: </label>
-                            <input type="text" name="nome" placeholder="Digite seu nome completo"> <br> <br>
-                        <label>E-mail: </label>
-                            <input type="email" name="email" placeholder="Digite seu e-mail"> <br> <br>
-                        <label>Senha: </label>
-                            <input type="password" name="senha" placeholder="Digite uma senha"> <br> <br>
-                        <input type="submit" value="Cadastrar"> <br>
+                <div class="shadow-lg p-3 mb-5 bg-white rounded" id="divsombra">
+                    <h4><b>Acessar como Administrador</b></h4><br>
+                    <form 1method="POST" id="formlogin">
+                        <input type="email" placeholder="E-mail" name="email"> <br><br>
+                        <input type="password" placeholder="Senha" name="senha"> <br><br>
+                        <input class="btn btn-success" type="submit" value="Log in"> <br><br>
                     </form>
                 </div>
             </center>
         </div>
     <div class="footer"></div>
     </body>
-</html>
+    </html>
--- cadastro_produto.php
+++ cadastro_produto.php
@@ -8,25 +8,62 @@
     <title> Cadastro </title>
 </head>
     <body>
-        <center>
-                <br> <br> <br> <h1>Cadastro de Medicamento</h1>
-            <form method="POST" action=".php">
-                    <label><b>Nome:</b></label>
-                    <input type="text" name="nome" placeholder="Nome do medicamento"><br><br>
-                    <label><b>Fórmula:</b></label>
-                <select id="sformula">
-                    <option value=""> Selecione </option>
-                    <option value="Original"> Original </option>
-                    <option value="Generico"> Genérico </option>
-                </select><br><br>
-                    <label><b>Preço:</b></label>
-                    <input type="number" name="preco" placeholder="Preço do medicamento"><br><br>
-                    <label><b>Quantidade:</b></label>
-                    <input type="number" name="quant" placeholder="Quantidade de medicamentos"><br><br>
-                    <label><b>Código:</b></label>
-                    <input type="number" name="cod" placeholder="Código do medicamento"><br><br>
-                    <input type="submit" value="Cadastrar medicamento"><br>
-            </form>
-        </center>
+    <div class="header">Pharma Store ✙</div>
+            <nav class="navbar navbar-expand-md">
+                <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <a href="index.php">
+                                <button type="button" id="paginicial" title="Voltar a página Inicial">
+                                    <svg class="bi bi-house-fill" width="20px" height="20px" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" d="M8 3.293l6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
+                                        <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
+                                    </svg>
+                                </button>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <button type="button" id="cart">
+                                <svg class="bi bi-cart3" width="20px" height="20px" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
+                                </svg>
+                            </button>
+                            <button type="button" id="logout" title="Logout">
+                                <svg class="bi bi-person-dash-fill" width="20px" height="20px" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm5-.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5z"/>
+                                </svg>
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <div class="content">
+                <center>
+                    <div class="shadow-lg p-3 mb-5 bg-white rounded" id="divsombra">    
+                    <h1>Cadastro de Medicamento</h1> <br>
+                        <form method="POST" action=".php">
+                            <label><b>Nome:</b></label>
+                                <input type="text" name="nome" placeholder="Nome do medicamento"><br><br>
+                            <label><b>Fórmula:</b></label>
+                                <select id="sformula">
+                                    <option value=""> Selecione </option>
+                                    <option value="Original"> Original </option>
+                                    <option value="Generico"> Genérico </option>
+                                </select><br><br>
+                            <label><b>Preço:</b></label>
+                                <input type="number" name="preco" placeholder="Preço do medicamento"><br><br>
+                            <label><b>Quantidade:</b></label>
+                                <input type="number" name="quant" placeholder="Quantidade de medicamentos"><br><br>
+                            <label><b>Código:</b></label>
+                                <input type="submit" value="Cadastrar medicamento"><br>
+                        </form>
+                    </div>
+                </center>
+            </div>
+        <div class="footer"></div>
     </body>
 </html>
